3edeb69003a8e73ba7d12b25026f88d3afb63d4e
koinu <ru.666.sz@gmail.com>
2026-03-11 19:31:44 +0900

    修復と改善：誤貼り付けの除去、トピックス更新、管理画面へのクリアボタン追加回

--- js/topics-data.js
+++ js/topics-data.js
@@ -2,6 +2,11 @@
 const topicsData = [
     {
         "date": "2026/03/11",
+        "content": "ハーモグロー会計事務所です！",
+        "url": "https://nmrn0629.github.io/-hp3/"
+    },
+    {
+        "date": "2026/03/11",
         "content": "ホームページを一部更新しました。",
         "url": ""
     },
