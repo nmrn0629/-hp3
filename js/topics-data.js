d5f8def9ca360b8b52456ea21e8167cd0786ef15
koinu <ru.666.sz@gmail.com>
2026-03-15 13:24:17 +0900

    更新：トピックスデータを整理し、開設メッセージに変更

--- js/topics-data.js
+++ js/topics-data.js
@@ -1,23 +1,8 @@
 /* Topics Content Data */
 const topicsData = [
     {
-        "date": "2026/03/11",
-        "content": "テスト2",
-        "url": "https://nmrn0629.github.io/-hp3/"
-    },
-    {
-        "date": "2026/03/11",
-        "content": "テスト",
-        "url": "https://nmrn0629.github.io/-hp3/"
-    },
-    {
-        "date": "2026/03/11",
-        "content": "ハーモグロー会計事務所です！",
-        "url": "https://nmrn0629.github.io/-hp3/"
-    },
-    {
-        "date": "2026/03/11",
-        "content": "ホームページを一部更新しました。",
+        "date": "2026/03/15",
+        "content": "ホームページを開設しました！",
         "url": ""
     }
 ];
