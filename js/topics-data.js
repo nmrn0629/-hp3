5136634bee86d3b51dd4ebfcbdd70f95e983356d
koinu <ru.666.sz@gmail.com>
2026-03-26 00:24:11 +0900

    Update TOPICS: 基礎控除の見直し（令和7・8年分）, 給与所得控除の見直し（令和7年分から）

--- js/topics-data.js
+++ js/topics-data.js
@@ -1,6 +1,21 @@
 /* Topics Content Data */
 const topicsData = [
     {
+        "date": "2026/03/25",
+        "content": "基礎控除の見直しについて（令和7年・8年分から大幅引き上げ）",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1199.htm"
+    },
+    {
+        "date": "2026/03/25",
+        "content": "給与所得控除の見直しについて（令和7年分から最低控除額が65万円に引き上げ）",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1410.htm"
+    },
+    {
+        "date": "2026/03/25",
+        "content": "消費税インボイス制度特設サイト",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/zeimokubetsu/shohi/keigenzeiritsu/invoice.htm"
+    },
+    {
         "date": "2026/03/15",
         "content": "ホームページを開設しました！",
         "url": ""
