54a5faaef675f1678bf2eb131dcaa262633182a4
宮脇名峰 <ah266@MacBook-Air.local>
2026-05-31 15:54:13 +0900

    Keep 12 months of TOPICS archives

--- js/topics-data.js
+++ js/topics-data.js
@@ -102,6 +102,55 @@ const topicsData = [
     }
 ];
 
+const topicsArchiveData = topicsData.concat([
+    {
+        "date": "2026/04/19",
+        "content": "「取引相場のない株式の評価に関する有識者会議」の開催について",
+        "url": "https://www.nta.go.jp/about/council/kenkyu.htm#nai-hyoka"
+    },
+    {
+        "date": "2026/04/01",
+        "content": "消費税法施行令の一部改正について",
+        "url": "https://www.nta.go.jp/law/tsutatsu/kihon/shohi/kaisei/0026003-067/index.htm"
+    },
+    {
+        "date": "2026/04/01",
+        "content": "通勤手当の非課税限度額の改正について",
+        "url": "https://www.nta.go.jp/users/gensen/2026tsukin/index.htm"
+    },
+    {
+        "date": "2026/03/28",
+        "content": "法人税基本通達の一部改正について（令和８年１月30日）",
+        "url": "https://www.nta.go.jp/law/joho-zeikaishaku/hojin/260130/index.htm"
+    },
+    {
+        "date": "2026/03/28",
+        "content": "電子帳簿保存法関係届出書等の様式の制定について（令和８年２月改正）",
+        "url": "https://www.nta.go.jp/law/tsutatsu/kobetsu/denshichoubo/index.htm"
+    },
+    {
+        "date": "2026/03/25",
+        "content": "基礎控除の見直しについて（令和7年・8年分から大幅引き上げ）",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1199.htm"
+    },
+    {
+        "date": "2026/03/25",
+        "content": "給与所得控除の見直しについて（令和7年分から最低控除額が65万円に引き上げ）",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1410.htm"
+    },
+    {
+        "date": "2026/03/25",
+        "content": "消費税インボイス制度特設サイト",
+        "url": "https://www.nta.go.jp/taxes/shiraberu/zeimokubetsu/shohi/keigenzeiritsu/invoice.htm"
+    },
+    {
+        "date": "2026/03/15",
+        "content": "ホームページを開設しました！",
+        "url": ""
+    }
+]);
+
 if (typeof window !== 'undefined') {
     window.topicsData = topicsData;
+    window.topicsArchiveData = topicsArchiveData;
 }
